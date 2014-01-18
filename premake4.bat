536b0bd82a97be7d98b9c3fd00f48f695de2fcff
Jonathan Bastnagel <jonthebagel@gmail.com>
2014-01-18 03:47:00 -0600

    Added Premake4 support for VS2010 and Codeblocks

--- premake4.bat
+++ premake4.bat
@@ -1,1 +1,3 @@
+premake4.exe vs2010
 premake4.exe vs2012
+premake4.exe codeblocks
--- premake4.lua
+++ premake4.lua
@@ -1,64 +1,68 @@
-solution "Mooball"
-	location "build"
-	
-	configurations { "Debug", "Release" }
-	platforms { "Native", "x32", "x64" }
-	
-	targetdir "bin"
-	
-local EXTLIBS = "extlibs"
+if _ACTION then
+	local BUILD_PATH = "build/" .. _ACTION
 
-local DLLName = "MooballDLL" 
-local WIN_SDK_DIR = "$(WindowsSdkDir)"
-local MEDIA = "bin/Media"
+	solution "Mooball"
+		location ( BUILD_PATH )
+		
+		configurations { "Debug", "Release" }
+		platforms { "Native", "x32", "x64" }
+		
+		targetdir "bin"
+		
+	local EXTLIBS = "extlibs"
 
-project (DLLName)
-	uuid "bf47d341-0a8d-4de3-a363-443648bf21af"
-	kind "WindowedApp"
-	language "C++"
-	location "build"
-	defines { "_MOOBALL_DLL", "WIN32", "__WIN32" }
+	local DLLName = "MooballDLL" 
+	local WIN_SDK_DIR = "$(WindowsSdkDir)"
+	local MEDIA = "bin/Media"
 
-	files { project().name .. "/include/**.h", project().name .. "/include/**.hpp", project().name .. "/src/**.cpp" }
-	
-	includedirs( project().name .. "/include" )
-	includedirs( WIN_SDK_DIR .. "Include" )
-	
-	implibdir ( project().name .. "/lib" )
-	libdirs { WIN_SDK_DIR .. "Lib" }
-	
-	vpaths	
-	{ 
-		["Headers/*"] =	
+	project (DLLName)
+		uuid "bf47d341-0a8d-4de3-a363-443648bf21af"
+		kind "WindowedApp"
+		language "C++"
+		location ( BUILD_PATH )
+		defines { "_MOOBALL_DLL", "WIN32", "__WIN32" }
+
+		files { project().name .. "/include/**.h", project().name .. "/include/**.hpp", project().name .. "/src/**.cpp" }
+		
+		includedirs( project().name .. "/include" )
+		includedirs( WIN_SDK_DIR .. "Include" )
+		
+		implibdir ( project().name .. "/lib" )
+		libdirs { WIN_SDK_DIR .. "Lib" }
+		
+		vpaths	
 		{ 
-			project().name .. "/include/**.h", 
-			project().name .. "/include/**.hpp" 
-		},
-		["Source/*"] =	
-		{ 
-			project().name .. "/src/**.c", 
-			project().name .. "/src/**.cpp" 
+			["Headers/*"] =	
+			{ 
+				project().name .. "/include/**.h", 
+				project().name .. "/include/**.hpp" 
+			},
+			["Source/*"] =	
+			{ 
+				project().name .. "/src/**.c", 
+				project().name .. "/src/**.cpp" 
+			}
 		}
-	}
-	
-	configuration { "Debug" }
-		debugdir "bin"
-		targetname ( project().name .. "_d" )
-		flags { "Symbols" }
-		defines { "_DEBUG" }
-		links { "OpenGL32", "glu32" }
 		
-		configuration { "Native" }			
-		configuration { "x32" }
-		configuration { "x64" }
+		configuration { "Debug" }
+			debugdir "bin"
+			targetname ( project().name .. "_d" )
+			flags { "Symbols" }
+			defines { "_DEBUG" }
+			links { "OpenGL32", "glu32" }
 			
-	configuration { "Release" }
-		debugdir "bin"
-		targetname ( project().name )
-		flags { "Optimize" }
-		defines { "_RELEASE" }
-		links { "OpenGL32", "glu32" }
-		
-		configuration { "Native" }
-		configuration { "x32" }
-		configuration { "x64" }
+			configuration { "Native" }			
+			configuration { "x32" }
+			configuration { "x64" }
+				
+		configuration { "Release" }
+			debugdir "bin"
+			targetname ( project().name )
+			flags { "Optimize" }
+			defines { "_RELEASE" }
+			links { "OpenGL32", "glu32" }
+			
+			configuration { "Native" }
+			configuration { "x32" }
+			configuration { "x64" }
+end
